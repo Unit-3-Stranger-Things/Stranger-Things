4fd510385adb5606480f77f8aa676af35e4fd894
mherdy2 <mherdy_2@hotmail.com>
2023-08-08 14:46:24 -0700

    maui edits

--- StrangerThings/src/components/Authorization/Register.jsx
+++ StrangerThings/src/components/Authorization/Register.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 
 function Register() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegister = () => {
@@ -11,7 +14,10 @@ function Register() {
   return (
     <div>
       <h2>Register</h2>
-      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="text" placeholder="text" value={name} onChange={e => setName(e.target.value)} />
+      <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="tel" placeholder="tel" value={phone} onChange={e => setPhone(e.target.value)} />
+      <input type="email" placeholder="Email" value={username} onChange={e => setUsername(e.target.value)} />
       <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
       <button onClick={handleRegister}>Register</button>
     </div>
--- StrangerThings/src/components/NavBar.jsx
+++ StrangerThings/src/components/NavBar.jsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
+    // navigation panel header
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/post">Post</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/logout">Sign Out</Link></li>              
       </ul>
     </nav>
   );
