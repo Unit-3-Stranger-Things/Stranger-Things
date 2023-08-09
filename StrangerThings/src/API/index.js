2fac8b4e7a7d9d1db4c7f2ff498ed4b0931df40b
mherdy2 <mherdy_2@hotmail.com>
2023-08-08 19:07:09 -0700

    maui edits2

--- StrangerThings/src/API/index.js
+++ StrangerThings/src/API/index.js
@@ -1,9 +1,60 @@
 const COHORT_NAME = '2302-acc-pt-web-pt-d';
-const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
-const API = () => {
-    return (  
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+const API_URL = {
+  login: async (email, password) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            email,
+            password,
+          },
+        }),
+      });
 
-    );
-}
- 
-export default API;
+      const data = await response.json();
+
+      if (response.ok) {
+        // Handle successful login
+        return data;
+      } else {
+        // Handle login failure
+        throw new Error(data.error.message);
+      }
+    } catch (error) {
+      throw new Error('An error occurred during login.');
+    }
+  },
+
+  register: async (userDetails) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: userDetails }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Handle successful registration
+        return data;
+      } else {
+        // Handle registration failure
+        throw new Error(data.error.message);
+      }
+    } catch (error) {
+      throw new Error('An error occurred during registration.');
+    }
+  },
+
+  // Add more API functions for various actions here
+};
+
+export default API_URL;
--- StrangerThings/src/components/Authorization/Login.jsx
+++ StrangerThings/src/components/Authorization/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import  API_URL from '../../API/index';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
